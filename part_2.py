3781fe2580d1e78c25f46c251b606c9421d3aae6
a <amanatman@gmail.com>
2021-04-06 19:42:01 +0530

    correct states

--- part_2.py
+++ part_2.py
@@ -40,7 +40,7 @@ ACTION_SHOOT = 6
 ACTION_CRAFT = 7
 ACTION_GATHER = 8
 ACTION_NONE = 9
-ACTION_ARR = ('UP', 'LEFT', 'DOWN', 'RIGHT', 'STAY', 'SHOOT', 'HIT', 'CRAFT', 'GATHER', 'NONE')
+ACTION_ARR = ('UP', 'LEFT', 'RIGHT', 'DOWN', 'STAY', 'HIT', 'SHOOT', 'CRAFT', 'GATHER', 'NONE')
 
 arr = [1/2, 1, 2]
 X = 26
@@ -75,29 +75,143 @@ def get_prob_next_state(s, a):
     """    
     s_a = deepcopy(s)
     s_b = deepcopy(s)
+
     if s.pos == POSITION_SOUTH:
+
         if a == ACTION_UP:
             s_a.pos = POSITION_CENTER
-            s_b.pos = POSITION_EAST
+            s_b.pos = POSITION_EAST # teleport
             return [[0.85, s_a], [0.15, s_b]]
+
         elif a == ACTION_STAY:
-            s_b.pos = POSITION_EAST    
+            s_b.pos = POSITION_EAST # teleport
             return [[0.85, s_a], [0.15, s_b]]
+
         elif a == ACTION_GATHER:
             s_a.mat = min(2, s_a.mat + 1)
             return [[0.75, s_a], [0.25, s_b]]
-        return None
+        return []
 
-def get_utility(prob_state, U):
+    elif s.pos == POSITION_NORTH:
+
+        if a == ACTION_DOWN:
+            s_a.pos = POSITION_CENTER
+            s_b.pos = POSITION_EAST
+            return [[0.85, s_a], [0.15, s_b]]
+        
+        elif a == ACTION_STAY:
+            s_b.pos = POSITION_EAST    
+            return [[0.85, s_a], [0.15, s_b]]
+        
+        elif a == ACTION_CRAFT:
+            if s.mat == 0:
+                return []
+            s_c = deepcopy(s)
+            s_a.mat -= 1
+            s_b.mat -= 1
+            s_c.mat -= 1
+            s_a.arrow = min(3, s_a.arrow + 1)             
+            s_b.arrow = min(3, s_b.arrow + 2)
+            s_c.arrow = min(3, s_c.arrow + 3)
+            return [[0.5, s_a], [0.35, s_b], [0.15, s_c]]
+        return []
+
+    elif s.pos == POSITION_EAST:
+        
+        if a == ACTION_LEFT:
+            s_a.pos = POSITION_CENTER
+            return [[1, s_a]]
+        
+        elif a == ACTION_STAY:
+            return [[1, s]]
+        
+        elif a == ACTION_SHOOT:
+            if s.arrow == 0:
+                return []
+            s_a.mm_health = max(0, s.mm_health - 1)
+            s.arrow -= 1
+            s_a.arrow -= 1
+            return [[0.9, s_a], [0.1, s]]
+        
+        elif a == ACTION_HIT:
+            s_a.mm_health = max(0, s.mm_health - 2)
+            return [[0.2, s_a], [0.8, s]]
+
+        return []
+
+    elif s.pos == POSITION_WEST:
+
+        if a == ACTION_RIGHT:
+            s_a.pos = POSITION_CENTER
+            return [[1, s_a]]
+        
+        elif a == ACTION_STAY:
+            return [[1, s]]    
+        
+        elif a == ACTION_SHOOT:
+            if s.arrow == 0:
+                return []
+            s.arrow -= 1
+            s_a.arrow -= 1
+            s_a.mm_health = max(0, s.mm_health - 1)
+            return [[0.25, s_a], [0.75, s]]
+        
+        return []
+
+    elif s.pos == POSITION_CENTER:
+        
+        if a == ACTION_DOWN:
+            s_a.pos = POSITION_SOUTH
+            s_b.pos = POSITION_EAST
+            return [[0.85, s_a], [0.15, s_b]]
+        
+        elif a == ACTION_LEFT:
+            s_a.pos = POSITION_WEST
+            s_b.pos = POSITION_EAST
+            return [[0.85, s_a], [0.15, s_b]]
+        
+        elif a == ACTION_RIGHT:
+            s_a.pos = POSITION_EAST
+            s_b.pos = POSITION_EAST
+            return [[0.85, s_a], [0.15, s_b]]
+        
+        elif a == ACTION_STAY:
+            s_b.pos = POSITION_EAST
+            return [[0.85, s], [0.15, s_b]]
+
+        elif a == ACTION_SHOOT:
+            if s.arrow == 0:
+                return []
+            s.arrow -= 1
+            s_a.arrow -= 1
+            s_a.mm_health = max(0, s.mm_health - 1)
+            return [[0.5, s_a], [0.5, s]]
+        
+        elif a == ACTION_HIT:
+            s_a.mm_health = max(0, s.mm_health - 2)
+            return [[0.1, s_a], [0.9, s]]
+
+        return []
+
+    raise ValueError
+
+def get_utility(prob_state, U, attacked=False, s=None):
     utility = STEP_COST
-    for p, s in prob_state:
-        utility += p * GAMMA * U[s.pos][s.mat][s.arrow][s.mm_state][s.mm_health]
+    if attacked:
+        s.mm_health = min(4, s.mm_health + 1)
+        utility += -40 + GAMMA * U[s.pos][0][0][MM_STATE_DORMANT][s.mm_health]
+    else:
+        for p, s in prob_state:
+            utility += p * GAMMA * U[s.pos][s.mat][s.arrow][s.mm_state][s.mm_health]
+            if s.mm_health == 0:
+                utility += 50
+    
     return utility
 
 def get_scaled_utility(U, s, a):
     total_utility = 0
     prob_state = get_prob_next_state(s, a)
-    if s.mm_state ==  MM_STATE_DORMANT:
+    if s.mm_state == MM_STATE_DORMANT:
 
         for i in range(len(prob_state)):
             prob_state[i][1].mm_state = MM_STATE_READY
@@ -106,16 +220,58 @@ def get_scaled_utility(U, s, a):
         for i in range(len(prob_state)):
             prob_state[i][1].mm_state = MM_STATE_DORMANT
         total_utility += 0.8 * get_utility(prob_state, U)
+    
+    elif s.mm_state == MM_STATE_READY:
+
+        total_utility += 0.5 * get_utility(prob_state, U)
+
+        for i in range(len(prob_state)):
+            prob_state[i][1].mm_state = MM_STATE_DORMANT
+
+        total_utility += 0.5 * get_utility(prob_state, U, True, s)
+
+    else:
+        raise ValueError
     return total_utility        
     
 U = np.zeros((POSITIONS_RANGE, MATERIALS_RANGE, ARROWS_RANGE, MM_STATE_RANGE, MM_HEALTH_RANGE))   
-# for state, U_s in np.ndenumerate(U):
-#     if state[0] == 0:
-#         continue
-#     s = State(*state)
-#     for action in range(NUM_ACTIONS):
-#         r = get_next_state_prob(s, action)
-#         if r:
-#             print(ACTION_ARR[action], r[0][0], r[0][1], r[1][0], r[1][1]) 
+P = np.full((POSITIONS_RANGE, MATERIALS_RANGE, ARROWS_RANGE, MM_STATE_RANGE, MM_HEALTH_RANGE), -1, dtype='int')   
 
-print(get_scaled_utility(U, State(POSITION_SOUTH, 0, 0, MM_STATE_DORMANT, 1), ACTION_UP))
+while True:
+    delta = np.NINF
+    U_next = np.zeros(U.shape)
+    for state, U_s in np.ndenumerate(U):
+        if state[4] == 0:
+            continue
+        U_s_next = np.NINF
+
+        s = State(*state)
+        for action in range(NUM_ACTIONS):
+            U_s_next = max(U_s_next, get_scaled_utility(U, s, action))
+
+        U_next[state] = U_s_next
+        delta = max(delta, abs(U_s_next - U_s))
+    
+    U = deepcopy(U_next)
+
+    if delta < DELTA:
+        break
+
+for state, U_s in np.ndenumerate(U):
+    if state[4] == 0:
+        continue
+    U_s_next = np.NINF
+    best_action = None
+
+    s = State(*state)
+    for action in range(NUM_ACTIONS):
+        cur_utility = get_scaled_utility(U, s, action)
+        if U_s_next < cur_utility:
+            U_s_next = cur_utility
+            best_action = action
+
+    P[state] = best_action
+
+for state, U_s in np.ndenumerate(U):
+    print(State(*state), U_s, ACTION_ARR[P[state]])
+# print(get_scaled_utility(U, State(POSITION_SOUTH, 0, 0, MM_STATE_DORMANT, 1), ACTION_UP))
