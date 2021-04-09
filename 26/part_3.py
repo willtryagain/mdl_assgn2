e98bc526b814fee59b1e3a3159d9527f17b10d82
a <amanatman@gmail.com>
2021-04-09 12:49:37 +0530

    ijjj

--- 26/part_3.py
+++ 26/part_3.py
@@ -9,6 +9,7 @@ MATERIALS_RANGE = 3
 ARROWS_RANGE = 4
 MM_STATE_RANGE = 2
 MM_HEALTH_RANGE = 5
+NUM_STATES = POSITIONS_RANGE * MATERIALS_RANGE * ARROWS_RANGE * MM_STATE_RANGE * MM_HEALTH_RANGE
 
 POSITIONS_VALUES = tuple(range(POSITIONS_RANGE))
 MATERIALS_VALUES = tuple(range(MATERIALS_RANGE))
@@ -113,33 +114,32 @@ class State:
     def get_scaled_distrib(self, distrib):
         next_distrib = []
 
-
         if self.mm_state == MM_STATE_READY:
             for p, s in distrib:
                 next_distrib.append((p*0.5, s))
-            if self.pos == POSITION_CENTER or state.pos == POSITION_EAST:
+            if self.pos == POSITION_CENTER or self.pos == POSITION_EAST:
                 next_distrib.append((1, State(self.pos, self.mat, 0, MM_STATE_DORMANT, min(4, self.mm_health + 1))))
+            else:
+                for p, s in distrib:
+                    next_distrib.append((p*0.5, State(s.pos, s.mat, s.arrow, MM_STATE_DORMANT, s.mm_health)))
 
+        elif self.mm_state == MM_STATE_DORMANT:
+            for p, s in distrib:
+                next_distrib.append((p*0.8, s))
+            for p, s in distrib:
+                next_distrib.append((p*0.2, State(s.pos, s.mat, s.arrow, MM_STATE_READY, s.mm_health)))    
 
-        
-        for state in states:
-            next_states.append(State(state.pos, state.mat, state.arrow, MM_STATE_DORMANT, state.mm_health))
-        return next_states
+        else:
+            raise ValueError
 
-    def get_prob_state(self, action):
+        return next_distrib
+
+    def get_distrib(self, action):
         if action not in self.get_actions():
             raise ValueError
 
         if action == ACTION_NONE:
             return []
-
-        if self.mm_state == MM_STATE_READY:
-
-            if self.pos == POSITION_CENTER:
-                return [
-                    (0.5, s_a),
-                    (0.5, s_b)
-                ]
 
         if action == ACTION_SHOOT:
             s_a = State(self.pos, self.mat, self.arrow - 1, self.mm_state, self.mm_health - 1)
@@ -165,12 +165,189 @@ class State:
                 raise ValueError 
 
         if action == ACTION_HIT:
-            s_a = State(self.pos, self.mat, self.arrow - 1, self.mm_state, self.mm_health - 1)
-            s_b = State(self.pos, self.mat, self.arrow - 1, self.mm_state, self.mm_health)
+            s_a = State(self.pos, self.mat, self.arrow, self.mm_state, max(0, self.mm_health - 2))
+            # s_b = State(self.pos, self.mat, self.arrow, self.mm_state, self.mm_health)
 
-            if self._po
+            if self.pos == POSITION_CENTER:
+                return [
+                    (0.1, s_a),
+                    # (0.9, s_b)
+                ]
+            elif self.pos == POSITION_EAST:
+                return [
+                    (0.2, s_a),
+                    # (0.1, s_b)
+                ]
+            else:
+                raise ValueError 
 
                 
-    
+        if action == ACTION_GATHER:
+
+            if self.mat == 2:
+                return []
+
+            s_a = State(self.pos, self.mat + 1, self.arrow, self.mm_state, self.mm_health)
+
+            if self.pos == POSITION_SOUTH:
+                return [
+                    (0.75, s_a)
+                ]
+
+            else:
+                raise ValueError 
+
+        if action == ACTION_CRAFT:
+
+            if self.arrow == 3:
+                return []
+
+            s_a = State(self.pos, self.mat, min(3, self.arrow + 1), self.mm_state, self.mm_health)
+            s_b = State(self.pos, self.mat, min(3, self.arrow + 2), self.mm_state, self.mm_health)
+            s_c = State(self.pos, self.mat, min(3, self.arrow + 3), self.mm_state, self.mm_health)
+
+
+            if self.pos == POSITION_NORTH:
+                return [
+                    (0.5, s_a),
+                    (0.35, s_b),
+                    (0.15, s_c)
+                ]
+
+            else:
+                raise ValueError 
+
+        if action == ACTION_STAY:
+
+            s_a = State(POSITION_EAST, self.mat, self.arrow, self.mm_state, self.mm_health)
+
+            if self.pos == POSITION_EAST or self.pos == POSITION_WEST:
+                return []
+
+            return [
+                (0.15, s_a)
+            ]
+
+        if action == ACTION_UP:
+
+            s_b = State(POSITION_EAST, self.mat, self.arrow, self.mm_state, self.mm_health)
+
+            if self.pos == POSITION_CENTER:
+                s_a = State(POSITION_NORTH, self.mat, self.arrow, self.mm_state, self.mm_health)
+                
+                return [
+                    (0.85, s_a),
+                    (0.15, s_b)
+                ]
+
+            elif self.pos == POSITION_SOUTH:
+                s_a = State(POSITION_CENTER, self.mat, self.arrow, self.mm_state, self.mm_health)
+                
+                return [
+                    (0.85, s_a),
+                    (0.15, s_b)
+                ]
+
+            else:
+                raise ValueError
+
+        if action == ACTION_DOWN:
+
+            s_b = State(POSITION_EAST, self.mat, self.arrow, self.mm_state, self.mm_health)
+
+            if self.pos == POSITION_CENTER:
+                s_a = State(POSITION_SOUTH, self.mat, self.arrow, self.mm_state, self.mm_health)
+                
+                return [
+                    (0.85, s_a),
+                    (0.15, s_b)
+                ]
+
+            elif self.pos == POSITION_NORTH:
+                s_a = State(POSITION_CENTER, self.mat, self.arrow, self.mm_state, self.mm_health)
+                
+                return [
+                    (0.85, s_a),
+                    (0.15, s_b)
+                ]
+
+            else:
+                raise ValueError
+
+        if action == ACTION_LEFT:
+
+            s_b = State(POSITION_EAST, self.mat, self.arrow, self.mm_state, self.mm_health)
+
+            if self.pos == POSITION_CENTER:
+                s_a = State(POSITION_WEST, self.mat, self.arrow, self.mm_state, self.mm_health)
+                
+                return [
+                    (0.85, s_a),
+                    (0.15, s_b)
+                ]
+
+            elif self.pos == POSITION_EAST:
+                s_a = State(POSITION_CENTER, self.mat, self.arrow, self.mm_state, self.mm_health)
+
+                return [
+                    (1, s_a),
+                ]
+                
+            else:
+                raise ValueError
+
+        if action == ACTION_RIGHT:
+
+            s_b = State(POSITION_EAST, self.mat, self.arrow, self.mm_state, self.mm_health)
+
+            if self.pos == POSITION_CENTER:
+                s_a = State(POSITION_EAST, self.mat, self.arrow, self.mm_state, self.mm_health)
+                
+                return [
+                    (0.85, s_a),
+                    (0.15, s_b)
+                ]
+
+            elif self.pos == POSITION_WEST:
+                s_a = State(POSITION_CENTER, self.mat, self.arrow, self.mm_state, self.mm_health)
+
+                return [
+                    (1, s_a),
+                ]
+                
+            else:
+                raise ValueError
+
+        raise ValueError
+
+
     def __str__(self):
         return f'({POSITION_ARR[self.pos]},{self.mat},{self.arrow},{MM_STATE_ARR[self.mm_state]},{self.mm_health * MM_HEALTH_FACTOR})'
+
+    @classmethod
+    def get_state_from_hash(self, index):
+        if type(index) != int:
+            raise ValueError
+
+        if not (0 <= index < NUM_STATES):
+            raise ValueError
+
+        pos = index // (MATERIALS_RANGE * ARROWS_RANGE * MM_STATE_RANGE * MM_HEALTH_RANGE)
+        index = index % (MATERIALS_RANGE * ARROWS_RANGE * MM_STATE_RANGE * MM_HEALTH_RANGE)
+
+        mat = index // (ARROWS_RANGE * MM_STATE_RANGE * MM_HEALTH_RANGE)
+        index = index % (ARROWS_RANGE * MM_STATE_RANGE * MM_HEALTH_RANGE)
+
+        arrow = index // (MM_STATE_RANGE * MM_HEALTH_RANGE)
+        index = index % (MM_STATE_RANGE * MM_HEALTH_RANGE)
+
+        mm_state = index // MM_HEALTH_RANGE
+        index = index % MM_HEALTH_RANGE
+
+        mm_health = index
+
+        return State(pos, mat, arrow, mm_state, mm_health)
+
+class IJ:
+
+    def __init__(self):
